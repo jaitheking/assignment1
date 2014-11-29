8bbf56ae222937af4614d8b21042f848f9b7ba38
jaitheking <jaitheking4858@gmail.com>
2014-11-29 13:24:38 +0800

    Working on the customer interface

--- customer.cpp
+++ customer.cpp
@@ -6,8 +6,13 @@
 
     }
 
-    string customer::get_cust_address(){
-        return cust_address;
+    string customer::get_cust_address1(){
+        return cust_address1;
+
+    }
+
+     string customer::get_cust_address2(){
+        return cust_address2;
 
     }
 
@@ -24,11 +29,19 @@
 
     }
 
-    void customer::set_cust_address(){
-            cout<<"Please enter your address:";
+    void customer::set_cust_address1(){
+            cout<<"Please enter your address (Line 1):";
 
-            getline(std::cin,cust_address);
-            this->cust_address=cust_address;
+            getline(std::cin,cust_address1);
+            this->cust_address1=cust_address1;
+
+    }
+
+    void customer::set_cust_address2(){
+            cout<<"Please enter your address (Line 2):";
+
+            getline(std::cin,cust_address2);
+            this->cust_address2=cust_address2;
 
     }
 
@@ -43,12 +56,58 @@
     void customer::display_userdetail(){
             cout<<"------Customer Details------"<<endl;
             cout<<"Name:"<<get_cust_name()<<endl;
-            cout<<"Address:"<<get_cust_address()<<endl;
+            cout<<"Address:"<<get_cust_address1()<<endl;
+            cout<<"Address:"<<get_cust_address2()<<endl;
             cout<<"Contact No:"<<get_cust_contact_no()<<endl;
 
     }
 
+    void customer::make_purchase(){
+                clearScreen()
+                CATEGORY:cout<<"Please select a category:"<<endl;
+                cout<<"1.Audio CDs"<<endl;
+                cout<<"2.Books"<<endl;
+                cout<<"3.Video Games"<<endl;
+
+                switch (choice1){
+                        case '1' :
+
+                               display_audio_cd();
+                               cout<<"Select the item you want:"<<endl;
+                               cin>>select_count;
+                               cout<<"Enter how many unit of the item do you want:"<<endl;
+                               cd_amount.push_back(quantity);
+                               order_cdlist->push_back(cdlist->begin()+select_count);
+                                cin.get();
+                                break;
+
+
+                    case '2'  :
+                                display_book();
+                               cout<<"Select the item you want:"<<endl;
+                               cin>>select_count;
+                               cout<<"Enter how many unit of the item do you want:"<<endl;
+                               book_amount.push_back(quantity);
+                               order_booklist->push_back(booklist->begin()+select_count);
+                                cin.get();
+                                break;
+
+                    case '3'  :
+                                display_game();
+                               cout<<"Select the item you want:"<<endl;
+                               cin>>select_count;
+                               cout<<"Enter how many unit of the item do you want:"<<endl;
+                               game_amount.push_back(quantity);
+                               order_gamelist->push_back(gamelist->begin()+select_count);
+                                cin.get();
+                                break;
+
+
+                }
+    }
+
     void customer::cust_menu(){
+            clearScreen();
             cout<<"What would you like to do?"<<endl;
             cout<<"1.Make a Purchase(s)"<<endl;
             cout<<"2.Checkout"<<endl;
@@ -57,21 +116,25 @@
             cin>>choice;
             switch (choice){
 
-                case 1 :
+                case '1' :
+                                make_purchase();
 
-                CATEGORY:cout<<"Please select a category:"<<endl;
-                cout<<"1.Books"<<endl;
-                cout<<"2.Audio CD"<<endl;
-                cout<<"3.Video Games"<<endl;
-                switch (choice1){
-                    case 1 :
+                                break;
 
-                            goto CATEGORY;
-
-                }
-                break;
+               case '2' :
+                                display_order();
+                                cout<<"Are you ready to checkout?"<<endl;
+                                cout<<"Ok.Now proceeding to external payment site...."<<endl;
+                                break;
 
             }
+
+
+    }
+
+    void customer::display_purchase{
+
+            cout<<"###########ORDER CONFIRMATION###########"<<endl;
 
 
     }
--- customer.hpp
+++ customer.hpp
@@ -4,21 +4,36 @@
 
     class customer : public store_user {
             string cust_name;
-            string cust_address;
+            string cust_address1;
+            string cust_address2;
             string cust_contact_no;
+            int select_count,quantity;
+            char choice
         public:
-            int choice;
-            int choice1;
+            char choice;
+            char choice1;
             string get_cust_name();
-            string get_cust_address();
+            string get_cust_address1();
+            string get_cust_address2();
             string get_cust_contact_no();
+            vector<book>* order_booklist=new vector<book>();
+            vector<book>::iterator orderbook_iter;
+            vector<int> bookquantity;
+            vector<audio_cd>* order_cdlist=new vector<audio_cd>();
+            vector<audio_cd>::iterator ordercd_iter;
+            vector<video_game>* order_gamelist=new vector<video_game>();
+            vector<video_game>::iterator ordergame_iter;
+            vector<int> cd_amount;
+            vector<int> book_amount;
+            vector<int> game_amount;
             void set_cust_name();
-            void set_cust_address();
+            void set_cust_address1();
+            void set_cust_address2();
             void set_cust_contact_no();
             void display_userdetail();
             void cust_menu();
-            void make_order();
-            void display_order();
+            void make_purchase();
+            void display_purchase();
 
     };
 #endif //CUSTOMER_HPP
